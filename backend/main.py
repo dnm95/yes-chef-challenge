fdf752ab2b3f033992eabc6b5443ecfe025b9535
Daniel Nava <dnmboy@gmail.com>
2026-02-03 14:42:50 -0600

    chore: deploy config for backend

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="Yes Chef API", version="1.0.0")
 
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["http://localhost:3000"],
+  allow_origins=["http://localhost:3000", "https://yes-chef-challenge.vercel.app"],
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
